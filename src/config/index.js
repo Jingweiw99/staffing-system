5cd64ef93c7a0a0b49eb2b92c0382df69624d25a
Wristwang <2117678336@qq.com>
2022-08-21 16:40:31 +0800

    环境配置的封装

---
+++ src/config/index.js
@@ -0,0 +1,25 @@
+/**
+ * 环境配置封装
+ */
+const env = import.meta.env.MODE || 'prod'
+
+const EnvConfig = {
+    dev: {
+        baseApi: '/',
+        mockApi: ''
+    },
+    test: {
+        baseApi: '//test.future.com/api',
+        mockApi: ''
+    },
+    prod: {
+        baseApi: '//future.com/api',
+        mock: ''
+    }
+}
+
+export default {
+    env,
+    mock:true,
+    ...EnvConfig[env]
+}
